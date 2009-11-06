a0596577640429341d877af333b904a299afec7b
Mika Tuupola <tuupola@appelsiini.net>
2009-11-07 01:17:15 +0200

    Song gets saved to database as string of 0 and 1's. Stubs for queue methods.

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,10 @@
-$('#editor img').click(function() {
-    $(this).toggleClass('play');
-    $(this).prev().click();
+
+$(function() {
+    $('input:checkbox:checked').each(function() {
+        $(this).next().addClass('play');        
+    });
+    $('#editor img').click(function() {
+        $(this).toggleClass('play');
+        $(this).prev().click();
+    });
 });
--- xylophone.rb
+++ xylophone.rb
@@ -7,6 +7,16 @@ require 'sequel'
 # Setup
 #
 
+DB = Sequel.sqlite('xylophone.db')
+
+DB.create_table? :songs do
+  primary_key :id
+  String :data
+  String :status
+end
+
+class Song < Sequel::Model
+end
 
 #
 # For arduino
@@ -14,10 +24,14 @@ require 'sequel'
 
 # Get list of songs in queue.
 get '/queue' do
+  @songs = Song.dataset.filter(:status => ['NEW']).order(:id)
+  erb :queue
 end
 
 # Get next song from queue.
 get '/next' do
+  @song = Song.dataset.filter(:status => ['NEW']).order(:id).first
+  erb :song
 end
 
 # Remove song from queue.
@@ -41,11 +55,14 @@ end
 
 # Save new song to dabase.
 post '/song' do
-  params['row'].inspect
+  @song = Song.create(:data => notes_to_binary_string_csv, :status => 'NEW')
+  erb :song
 end
 
 # Show song page.
 get '/song/:id' do
+  @song = Song[params[:id]]
+  erb :song
 end
 
 get '/about' do
@@ -55,4 +72,34 @@ end
 not_found do
   'No presents for bad children.'
 end
+
+#
+# Helper functions
+#
+
+helpers do
+  
+  def notes_to_binary_string_array
+    rows = []
+    
+    params['notes'].each do |row, row_notes| 
+      binary = '000000000000000'
+      row_notes.each do |key, value|
+        binary[key.to_i] = value
+      end 
+      rows[row.to_i] = '0b0' << binary
+    end
+    # Convert any nil to binary zero
+    rows.map! do |binary|
+      binary ||= '0b0000000000000000'
+    end
+    rows
+  end
+  
+  def notes_to_binary_string_csv
+    notes_to_binary_string_array.join(',')
+  end
+  
+end
+
 
