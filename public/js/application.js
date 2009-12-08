991aed1d7188e980c7c4d70730229356af7d7268
Mika Tuupola <tuupola@appelsiini.net>
2009-12-08 22:15:42 +0200

    Add effects to autogrow editor. Add FlowPlayer.

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,34 @@
 
 $(function() {
+
+    /* For the pages were player should be displayed. */
+    if ($('#player').size()) {
+        flowplayer("player", "/swf/flowplayer-3.1.5.swf");         
+    }
         
     $('input:checkbox:checked').each(function() {
         $(this).next().addClass('play');        
     });
     
     $('.editor img').click(function() {
-        console.log($(this).prev().val());
+        //console.log($(this).prev().val());
         
-        var notes_before = $(this).parent().children(':checked');
+        var notes_before = $(this).parent().children('input:checkbox:checked');
+        
+        /* Check the checkbox so note will be played.  */
+        /* Rest of the code is just to make sure there */
+        /* is only two active notes per row.           */
         $(this).toggleClass('play');
         $(this).prev().click();
-        var notes_after = $(this).parent().children(':checked');
+        
+        var notes_after = $(this).parent().children('input:checkbox:checked');
+
+        /* Make sure there is only two active notes per row. */
         if (2 < notes_after.size()) {
             notes_before.filter(':first').next('img').click();
         }
         
-        /* If we are at last row add a new one. */
+        /* If we are at last or sedond last row add a new row. */
         if (0 == $(this).parent().next('span').size()) {
             var num_rows = $('#editor_form > span').size();
             var new_row  = $(this).parent().clone(true);
@@ -27,7 +39,10 @@ $(function() {
             })
             $('img', new_row).removeClass('play');
             $('input', new_row).removeAttr('checked');
+            new_row.css('display', 'none');
+            
             new_row.insertAfter($(this).parent());
+            new_row.fadeIn('fast');
         }
         
     });
